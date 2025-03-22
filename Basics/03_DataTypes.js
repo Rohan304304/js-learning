3bb540ecb01ab99dd6e7cf2a67c4904db6895c84
Rohan304304 <22bce304@nirmauni.ac.in>
2025-03-22 10:12:12 +0000

    DataType Summary

---
+++ Basics/03_DataTypes.js
@@ -0,0 +1,69 @@
+let name = "Rohan"
+let age = 22
+let isLogged = true
+
+// null // standalone value
+// undefined
+// symbol // unique
+// object
+// console.log(typeof undefined)
+// console.log(typeof null);
+
+
+
+
+// CONVERSATION 
+
+let score = "334hj"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let scoreInNumber = Number(score)
+// console.log(scoreInNumber);
+// console.log(typeof(scoreInNumber))
+
+//33 => 33
+//"334jh" =>NaN
+//true =>1 and false => 0
+
+
+let log = 1;
+let Booleanlog = Boolean(log)
+// console.log(Booleanlog);
+
+//1 = true and 0 => false
+//"" => false
+//"string" =>true
+
+let str = 33
+let strString = String(str)
+// console.log(strString);
+// console.log(typeof strString);
+
+
+//OPERATIONS
+let value = 4
+let negValue = -value
+// console.log(negValue)
+// console.log(2 + 2 );
+
+let str1 = "hi"
+let str2 = " rohan"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 1);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1 + 2 + "3");
+console.log(1 + 2 + "3" + 1 + 2 );
+
+
+
+
+
+
+
+
+
