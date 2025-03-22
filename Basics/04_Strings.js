d1697ea5b471deb4b0ae0dc179d0805cb4f6716c
Rohan304304 <22bce304@nirmauni.ac.in>
2025-03-22 11:47:06 +0000

    Strings

---
+++ Basics/04_Strings.js
@@ -0,0 +1,18 @@
+const name = "Rohan"
+const age = 22
+
+// console.log(name + age + " Nimra Uni."); //old method
+
+//Modern way
+
+// console.log(`My name is ${name} and my age is ${age}.`); // string interpolation
+
+//String Methods
+const str = new String('rohan')
+console.log(str[3]);
+console.log(str.length);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str);
+
+
