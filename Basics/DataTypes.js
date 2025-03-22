7666f9a97cd0adde67239ad660a954567a08299d
Rohan304304 <22bce304@nirmauni.ac.in>
2025-03-22 07:51:30 +0000

    DataTypes and Variables

---
+++ Basics/DataTypes.js
@@ -0,0 +1,10 @@
+let name = "Rohan"
+let age = 22
+let isLogged = true
+
+// null // standalone value
+// undefined
+// symbol // unique
+// object
+console.log(typeof undefined)
+console.log(typeof null);
