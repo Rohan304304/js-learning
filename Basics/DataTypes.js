b0f84e2f01b8842a55482fbac6605eb8322cb790
Rohan304304 <22bce304@nirmauni.ac.in>
2025-03-22 08:18:56 +0000

    Conversation of data types

--- Basics/DataTypes.js
+++ Basics/DataTypes.js
@@ -8,3 +8,39 @@ let isLogged = true
 // object
 console.log(typeof undefined)
 console.log(typeof null);
+
+
+
+
+// CONVERSATION AND OPERATIONS
+
+let score = "334hj"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let scoreInNumber = Number(score)
+console.log(scoreInNumber);
+console.log(typeof(scoreInNumber))
+
+//33 => 33
+//"334jh" =>NaN
+//true =>1 and false => 0
+
+
+let log = 1;
+let Booleanlog = Boolean(log)
+console.log(Booleanlog);
+
+//1 = true and 0 => false
+//"" => false
+//"string" =>true
+
+let str = 33
+let strString = String(str)
+console.log(strString);
+console.log(typeof strString);
+
+
+
+
