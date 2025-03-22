bcb56e06148e461b2f1c2812646955cbeba43387
Rohan304304 <22bce304@nirmauni.ac.in>
2025-03-22 13:19:26 +0000

    Strings Methods1

--- Basics/04_Strings.js
+++ Basics/04_Strings.js
@@ -8,11 +8,27 @@ const age = 22
 // console.log(`My name is ${name} and my age is ${age}.`); // string interpolation
 
 //String Methods
-const str = new String('rohan')
-console.log(str[3]);
-console.log(str.length);
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str);
+const str = new String('roh-roh-an')
+// console.log(str[3]);
+// console.log(str.length);
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str);
+
+
+// console.log(str.search("han"))
+// console.log(str.slice(-3));
+// console.log(str.slice(2,5));
+console.log(str.split('-'));
+console.log(str.split('-',2));
+console.log(str.split('-',1));
+
+
+
+
+
+
+
+
 
 
