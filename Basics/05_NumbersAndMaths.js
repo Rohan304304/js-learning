a814992d0468878d30a2a97e4edb9025a726e227
Rohan304304 <22bce304@nirmauni.ac.in>
2025-03-23 04:43:32 +0000

    Numbers and Maths methods

---
+++ Basics/05_NumbersAndMaths.js
@@ -0,0 +1,41 @@
+//NUMBERS---------------------------------
+const age = 22;
+// console.log(age);
+
+const runs = new Number(100.89);
+// console.log(runs);
+
+// console.log(runs.toString().charAt(0));
+
+// console.log(runs.toFixed(5));
+// console.log(runs.toFixed(1));
+
+// console.log(runs.toPrecision(2));
+
+
+// console.log(runs.valueOf());
+// console.log(runs.toExponential(1/2));
+
+
+const other = 1000000
+// console.log(other.toLocaleString('en-IN'));
+
+// MATHS-----------------------------------
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.9)
+
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()*10))
+
+const min = 50
+const max = 200
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+
+
+
